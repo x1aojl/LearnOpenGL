a99436eb817b6cd9fe383c09f8da7d20489cae6e
x1aojl <x1aojl@icloud.com>
2021-10-19 21:25:05 +0800

    添加矩形颜色属性

--- Project/main.cpp
+++ Project/main.cpp
@@ -10,16 +10,19 @@
 const char *vertexShaderSource =
     "#version 330 core\n"
     "layout(location = 0) in vec3 aPos;\n"
+    "layout(location = 1) in vec3 aColor;\n"
+    "out vec4 ourColor;\n"
 
     "void main()\n"
     "{\n"
     "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "    ourColor = vec4(aColor, 1.0f);\n"
     "}\n";
 
 // 片断着色器源码
 const char *fragmentShaderSource =
     "#version 330 core\n"
-    "uniform vec4 ourColor;\n"
+    "in vec4 ourColor;\n"
     "out vec4 fragColor;\n"
 
     "void main()\n"
@@ -88,10 +91,11 @@ int main()
 
     // 初始化顶点数据
     float vertices[] = {
-         0.5f,  0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-        -0.5f,  0.5f, 0.0f
+        // ---- 位置 ----   // ---- 颜色 ----
+         0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // 右上
+         0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // 右下
+        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // 左下
+        -0.5f,  0.5f, 0.0f, 0.0f, 1.0f, 1.0f, // 左上
     };
 
     // 初始化索引数组
@@ -117,9 +121,13 @@ int main()
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    // 设置顶点属性指针
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    // 设置顶点位置属性指针
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    // 设置顶点颜色属性指针
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
